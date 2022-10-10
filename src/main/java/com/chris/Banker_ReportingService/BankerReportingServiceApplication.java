11b6fd0b222d1c733ac0d6d799ea2f19808d42f5
chris-mwesigwa <cmwesigwa@aetotechs.com>
2022-10-10 15:21:04 +0300

    innital commit

---
+++ src/main/java/com/chris/Banker_ReportingService/BankerReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_ReportingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankerReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankerReportingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/Banker_ReportingService/BankerReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_ReportingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
